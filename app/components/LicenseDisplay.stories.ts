5264da2494c17ca3deb247cd41e5438eb3b38a68
Scott Wu <sw@scottwu.ca>
2026-02-09 20:47:48 +0800

    add story: license

---
+++ app/components/LicenseDisplay.stories.ts
@@ -0,0 +1,18 @@
+import type { Meta, StoryObj } from '@nuxtjs/storybook'
+import Component from './LicenseDisplay.vue'
+
+const meta = {
+  component: Component,
+  tags: ['autodocs'],
+  argTypes: {
+    license: {
+      control: { type: 'text' },
+    },
+  },
+} satisfies Meta<typeof Component>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = { args: { license: 'MIT' } }
+export const Invalid: Story = { args: { license: 'invalid license' } }
